92ab2e4fab613208bff6057b6988bc7852a23919
git-jyoti <122430744+git-jyoti@users.noreply.github.com>
2026-08-05 21:39:57 +0530

    Update main.tf

--- infrastructure/modules/terraform_state/main.tf
+++ infrastructure/modules/terraform_state/main.tf
@@ -33,12 +33,12 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
- name         = var.table_name
+  name         = var.table_name
   billing_mode = "PAY_PER_REQUEST"
- hash_key     = "LockID"
+  hash_key     = "LockID"
 
   attribute {
     name = "LockID"
     type = "S"
   }
- }
+}
