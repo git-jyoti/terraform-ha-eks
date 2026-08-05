0822b5e72875623907c0e918e2eec7c093085c27
git-jyoti <122430744+git-jyoti@users.noreply.github.com>
2026-08-05 08:45:40 +0530

    Update main.tf

--- infrastructure/modules/iam/main.tf
+++ infrastructure/modules/iam/main.tf
@@ -9,42 +9,36 @@ data "aws_iam_role" "nodes" {
   name = "${var.project_name}-node-role"
 }
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      },
-    ]
-  })
+/* # Existing EKS Cluster IAM Role
+data "aws_iam_role" "cluster" {
+  name = "${var.project_name}-cluster-role"
 }
 
-resource "aws_iam_role_policy_attachment" "cluster_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.cluster.name
-}
 
-# EKS Node Group Role
-resource "aws_iam_role" "nodes" {
+# Existing EKS Node Group IAM Role
+data "aws_iam_role" "nodes" {
   name = "${var.project_name}-node-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
 }
+
+# Commented line no:13 to 24 because using existing role - data instead of resource
+  #assume_role_policy = jsonencode({
+  # Version = "2012-10-17"
+  # Statement = [
+  #   {
+  #     Action = "sts:AssumeRole"
+  #     Effect = "Allow"
+  #     Principal = {
+  #        Service = "eks.amazonaws.com"
+  #      }
+  #   },
+  # ]
+  #})
+#}
+
+#resource "aws_iam_role_policy_attachment" "cluster_policy" {
+#  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+#  role       = aws_iam_role.cluster.name
+#}
 
 resource "aws_iam_role_policy_attachment" "nodes_AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
@@ -59,4 +53,4 @@ resource "aws_iam_role_policy_attachment" "nodes_AmazonEKS_CNI_Policy" {
 resource "aws_iam_role_policy_attachment" "nodes_AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.nodes.name
-}
+} */
