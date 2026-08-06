3a207a24fcd7d85582395f2c719a9548a2e385ca
git-jyoti <122430744+git-jyoti@users.noreply.github.com>
2026-08-06 07:02:51 +0530

    Update main.tf

--- infrastructure/modules/terraform_state/main.tf
+++ infrastructure/modules/terraform_state/main.tf
@@ -24,7 +24,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "public_access" {
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
   bucket                  = aws_s3_bucket.terraform_state.id
   block_public_acls       = true
   block_public_policy     = true
@@ -32,7 +32,7 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   restrict_public_buckets = true
 }
 
-resource "aws_dynamodb_table" "terraform_locks" {
+resource "aws_dynamodb_table" "terraform_locks_table" {
   name         = var.table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
