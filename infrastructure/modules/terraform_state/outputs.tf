7953ad64157748c2d69f29dec16193f439356cf3
git-jyoti <122430744+git-jyoti@users.noreply.github.com>
2026-08-06 07:16:03 +0530

    Update outputs.tf

--- infrastructure/modules/terraform_state/outputs.tf
+++ infrastructure/modules/terraform_state/outputs.tf
@@ -3,7 +3,7 @@ output "bucket_name" {
   description = "The name of the S3 bucket"
 }
 
-output "table_name" {
-  value       = aws_dynamodb_table.terraform_locks.name
-  description = "The name of the DynamoDB table"
-}
+//output "table_name" {
+//  value       = aws_dynamodb_table.terraform_locks.name
+//  description = "The name of the DynamoDB table"
+//}
